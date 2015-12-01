306f79c8717860585787842b5d8b790ddbf776a8
Paul <sesshoumaru404@outlook.com>
2015-12-01 00:57:54 -0500

    working search

--- js/wiki.js
+++ js/wiki.js
@@ -1,60 +1,79 @@
-var newStyle = function (item) {
+function newStyle (number, item) {
   var wrapper = document.createElement("DIV");
-  wrapper.className = "wrapper";
-  wrapper.innerHTML = ''+
-'  <div class="col-md-6"><div class="card"> '+
-'    <div class="card__header"><img src="' + item.author.picture + '"></div> '+
-'    <div class="card__image card__image"><i class="fa fa-thumbs-o-up"></i></div> '+
-'    <div class="card__info"> '+
-'      <div class="details">'+
-'        <a href="' + item.link + '"><span class="details__text-header">'+ item.headline +'</span></a> '+
-'      </div> '+
-'      <div class="card__info__section-3"> '+
-'        <div class="details-bottom"> '+
-'          <span class="details__text">Rank</span> '+
-'          <span class="details__number">' + item.rank + '</span> '+
-'        </div> '+
-'      </div> '+
-'      <div class="card__info__section-3"> '+
-'        <div class="details-bottom"> '+
-'          <span class="details__text">Posted</span> '+
-'          <span class="details__number">' + moment(item.timePosted).format('LL') + '</span> '+
-'        </div> '+
-'      </div> '+
-'      <div class="card__info__section-3"> '+
-'        <div class="details-bottom"> '+
-'          <span class="details__text">Poster</span> '+
-'          <span class="details__number"><a href="http://www.freecodecamp.com/' + item.author.username + '">' + item.author.username + '</a></span> '+
-'        </div> '+
-'      </div> '+
-'    </div> '+
-'  </div></div>';
+  // wrapper.className = "entries";
+  wrapper.className = "entries";
+  wrapper.setAttribute("href", "https://en.wikipedia.org/wiki/"+ encodeURIComponent(item.title));
+  wrapper.innerHTML = '<span class="title">'+ item.title + '</span>' +
+  '<p>'+ item.snippet + '</p>';
   return wrapper;
-};
-
-function Wiki (search) {
-  this.link = "https://en.wikipedia.org/w/api.php?action=query&titles=" + search + "&prop=revisions&rvprop=content&format=json";
-  // console.log(this.link);
-  this.getLastest = function () {
-    $.ajax({
-        url: this.link,
-        type: 'GET',
-        dataType: 'jsonp',
-    }).done(function (data) {
-        console.log(data);
-        $('.results').append(data.query.pages);
-      }
-    );
-  };
 }
 
-$(document).ready(function(e) {
+function formatResults (data,search, add) {
+  // console.log(data);
+  // If results div empty
+  if ($.trim( $('.results').text() ).length !== 0 && add) {
+    $('.results').html('');
+  }
+  for(i =0; i < data.query.search.length; i++) {
+    $('.results').append(newStyle(i, data.query.search[i]));
+  }
+  if (data.continue) {
+    $('.moreResults').attr('start' , data.continue.sroffset);
+    $('.moreResults').attr('text', search);
+    $('.moreResults').show();
+  }
+}
 
-  $('input').keypress(function (e) {
+function Wiki (argument) {
+    this.test = function (argument) {
+      console.log(argument);
+    };
+
+    this.more = function(button) {
+      var more = $(button);
+      var start = more.attr('start');
+      var text = more.attr('text');
+      this.getSearch(text, start);
+    };
+
+    this.getSearch = function (search, moreResults) {
+      var link = "https://en.wikipedia.org//w/api.php?action=query&list=search&format=json&srsearch=" + search;
+      if (moreResults) {
+        link = link + "&sroffset=" + moreResults;
+      }
+      $.ajax({
+          url: link,
+          type: 'GET',
+          dataType: 'jsonp',
+      }).done(function (data) {
+          if (moreResults) {
+            formatResults(data, search, false);
+          } else {
+            formatResults(data, search);
+          }
+        }
+      );
+    };
+}
+
+
+$(document).ready(function(e) {
+  var results = new Wiki();
+  $('#wikisearch').keypress(function (e) {
     if (e.which == 13) {
-      var results = new Wiki(this.value);
-      results.getLastest();
+      results.getSearch(this.value);
       return false;    //<---- Add this line
     }
   });
+
+  $('.moreResults').click(function () {
+    results.more(this);
+  });
+
+  $('.results').on('click', '.entries', function () {
+    // console.log(this, arguments);
+    var link = $(this).attr('href');
+    window.location.href = link;
+  });
+
 });
