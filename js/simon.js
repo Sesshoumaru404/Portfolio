0e5bc1058f25372124722ee88f7fe3494c8186c0
Paul <sesshoumaru404@outlook.com>
2015-12-31 01:11:56 -0500

    design complete

--- js/simon.js
+++ js/simon.js
@@ -2,11 +2,16 @@ function Simon (div) {
   "use strict";
   this.gamediv = div;
   this.currentpattern = [];
-  this.gameboard = document.getElementById('tutorial');
-  this.gameOptions = ['red', 'white', 'blue'];
-  this.gamepaused = false;
+  this.gameOptions = ['red', 'green', 'blue', 'yellow'];
+  this.gamepaused = true;
   this.playcount = 0;
   this.checkcount = 0;
+  this.speed = 1200;
+  this.count = $(".count");
+  this.colorblue = $("path#blue").attr('fill');
+  this.colorred = $("path#red").attr('fill');
+  this.colorgreen = $("path#green").attr('fill');
+  this.coloryellow = $("path#yellow").attr('fill');
   this.notes2 = [
     "E", "E", "F", "G", "G", "F", "E", "D", "C", "C", "D", "E", "E", "D", "D"
   ];
@@ -14,17 +19,29 @@ function Simon (div) {
     "G", "E", "C", "E", "G", "C", "E", "D", "C", "E", "F", "G",
     "G", "G", "E", "D", "C", "B", "A", "B", "C", "C", "E","G", "C"
   ];
-  this.drawboard();
+  // this.drawboard();
   // this.test = "sdfsdfsdf0";
-    this.gamediv.on('click', '#start' ,
-      $.proxy(this.startgame, this)
-    );
+    this.gamediv.on('click', '.starts' ,
+    $.proxy(this.startgame, this)
+  );
 
-  if (!this.gamepaused) {
-    this.gamediv.on('click', '.simonbutton' ,
-      $.proxy(this.buttonclick, this)
+  // console.log(this['playcount']);
+  if (this.gamepaused) {
+    this.gamediv.on('mousedown', '.simonbutton' ,
+      $.proxy(this.glowstart, this)
     );
   }
+
+  this.gamediv.on('mouseup', '.simonbutton' ,
+    $.proxy(this.glowend, this)
+  );
+  this.gamediv.on('mouseout', '.simonbutton' ,
+    $.proxy(this.glowend, this)
+  );
+
+  // this.gamediv.on('click', '.simonbutton' ,
+  //   $.proxy(this.buttonclick, this)
+  // );
 }
 
 Simon.prototype.startgame = function (e) {
@@ -32,10 +49,8 @@ Simon.prototype.startgame = function (e) {
   return this.nextpattern();
 };
 
-Simon.prototype.buttonclick = function (e) {
-  this.gamepaused = true;
-  var simonbutton =  $(e.currentTarget).attr('id');
-
+Simon.prototype.glowstart = function (e) {
+  simonbutton =  $(e.currentTarget).attr('id');
   this.buttoneffect(simonbutton);
   if (this.checkentry(simonbutton)) {
     console.log(this.checkcount);
@@ -48,8 +63,26 @@ Simon.prototype.buttonclick = function (e) {
   } else {
     return this.playback();
   }
+  // this.currentpattern.push(nextpattern);
+};
 
-  console.log(this.currentGame);
+Simon.prototype.buttonclick = function (e) {
+  this.gamepaused = true;
+  var simonbutton =  $(e.currentTarget).attr('id');
+  this.buttoneffect(simonbutton);
+  if (this.checkentry(simonbutton)) {
+    console.log(this.checkcount);
+    console.log(this.currentpattern.length);
+    if  (this.checkcount === this.currentpattern.length) {
+      this.checkcount = 0;
+      return this.nextpattern();
+    }
+    return false;
+  } else {
+    return this.playback();
+  }
+  //
+  // console.log(this.currentGame);
     // $('.simonbutton').removeClass('clicked');
 };
 
@@ -57,29 +90,39 @@ Simon.prototype.nextpattern = function () {
   var nextpattern = Math.floor(Math.random() * this.gameOptions.length );
   nextpattern = this.gameOptions[nextpattern];
   this.currentpattern.push(nextpattern);
+  this.count.text(this.currentpattern.length);
   this.playcount = 0;
   this.playpattern();
 };
 
 Simon.prototype.playpattern = function () {
   var that = this;
-  this.deferredresume = $.Deferred();
+  // this.deferredresume = $.Deferred();
 
-  setTimeout($.proxy(this.playback, this), 1200);
+  setTimeout($.proxy(this.playback, this), this.speed);
 
-  return  this.deferredresume.promise();
+  // return  this.deferredresume.promise();
 };
 
 Simon.prototype.playback = function () {
-   this.buttoneffect(this.currentpattern[this.playcount]);
+  // console.log();
+  var id = this.currentpattern[this.playcount];
+  var color = this['color' + id];
+   this.buttoneffect(id);
    this.playcount++;
+   setTimeout(function (){
+    $("path#" + id).attr('fill', color);
+  }, 500);
    if (this.playcount < this.currentpattern.length) {
        return this.playpattern();
    } else if (this.playcount === this.currentpattern.length) {
-     console.log("hey");
+     // speed not fast that 500 
+     this.speed = this.speed -100;
+     $('.speed').text(this.speed );
+    //  console.log("hey");
      this.gamepaused = false;
    }else {
-     setTimeout(this.gamepaused = false, 1500);
+     setTimeout(this.gamepaused = false, this.speed + 300);
    }
 };
 
@@ -95,99 +138,28 @@ Simon.prototype.checkentry = function (simonbutton) {
   }
 };
 
+Simon.prototype.glowend = function (e) {
+  simonbutton =  $(e.currentTarget).attr('id');
+  $("path#" + simonbutton).attr('fill', this['color' + simonbutton]);
+};
+
 Simon.prototype.buttoneffect = function (id) {
-  $("#" + id).addClass('clicked');
+  this.resetcolor = $("path#" + id).attr('fill');
+  $("path#" + id).attr('fill', 'url(#Gradient'+ id +')');
   this.sounds(id);
-  setTimeout(function (){
-    $("#" + id).removeClass('clicked');
-  }, 200);
-  // this.currentpattern.push(nextpattern);
 };
 
 Simon.prototype.sounds = function (sound) {
+  console.log(sound);
   var audioElement = document.createElement('audio');
-  audioElement.setAttribute('src', '../files/notes/simonSound' + sound.toLowerCase() + '.mp3');
-  audioElement.setAttribute('autoplay', 'autoplay');
-  //audioElement.load()
+  audioElement.setAttribute('src', '../files/notes/simonSound' + sound + '.mp3');
+  // audioElement.setAttribute('autoplay', 'autoplay');
+  // audioElement.durationchange(5000);
+  audioElement.play();
+  // audioElement.loop = true;
 };
 
-// Drawing code below
 
-Simon.prototype.drawboard = function (argument) {
-  if (  this.gameboard.getContext){
-    var ctx = this.gameboard.getContext('2d');
-    var bordercolor = "white";
-    console.log("draw");
-
-    var bordercircle = new Path2D();
-    bordercircle.moveTo(125, 35);
-    bordercircle.arc(225, 225, 210, 0, 2 * Math.PI);
-    // bordercircle.fill();
-
-    var gamecirle = new Path2D();
-    // bordercircle.moveTo(125, 35)
-    gamecirle.arc(225, 225, 100, 0, 2 * Math.PI);
-
-    var gamecirleborder = new Path2D();
-    // bordercircle.moveTo(125, 35)
-    gamecirleborder.arc(225, 225, 110, 0, 2 * Math.PI);
-
-
-    var border = new Path2D();
-    // bordercircle.moveTo(125, 35)
-    border.arc(225, 225, 220, 0, 2 * Math.PI);
-
-    ctx.fillStyle = bordercolor;
-    ctx.fill(border);
-    // ctx.globalCompositeOperation = 'destination-over';
-
-    //yellow
-    ctx.fillStyle = bordercolor;
-    ctx.fillRect (5, 5, 220, 220);
-    ctx.fillStyle = "rgba(226, 220, 21, 1)";
-    ctx.fillRect (5, 5, 215, 215);
-
-    //red
-    ctx.fillStyle = bordercolor;
-    ctx.fillRect (225, 5, 220, 220);
-    ctx.fillStyle = "rgba(206, 12, 12, 1)";
-    ctx.fillRect (230, 5, 215, 215);
-
-    //blue
-    ctx.fillStyle = bordercolor;
-    ctx.fillRect (5, 225, 220, 220);
-    ctx.fillStyle = "rgba(28, 38, 60, 1)";
-    ctx.fillRect (0, 230, 220, 220);
-
-    //green
-    ctx.fillStyle = bordercolor;
-    ctx.fillRect (225, 225, 220, 220);
-    ctx.fillStyle = "rgba(14, 243, 25, 1)";
-    ctx.fillRect (230, 230, 220, 220);
-    // ctx.fill(yellowcircle);
-    // ctx.fillStyle = "rgb(200,0,0)";
-    // ctx.fillRect (10, 10, 55, 50);
-
-
-    // ctx.arc(200, 200, 180, 0, 2 * Math.PI);
-    ctx.globalCompositeOperation = 'destination-in';
-    ctx.fillStyle = "rgba(0, 0, 0, 1)";
-    ctx.fill(bordercircle);
-
-    // ctx.arc(200, 200, 100, 0, 2 * Math.PI);
-    ctx.globalCompositeOperation = 'source-over';
-    ctx.fillStyle = bordercolor;
-    ctx.fill(gamecirleborder);
-
-    ctx.fillStyle = "white";
-    ctx.fill(gamecirle);
-
-    // ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
-    // ctx.fillRect (30, 30, 55, 50);
-  } else {
-    $("#game").show();
-  }
-};
 
 
 
